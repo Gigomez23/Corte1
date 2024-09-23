4b098cf1e03a191bac002da42dd714d717d5b333
Gogogomez23 <com.gomez.com@gmail.com>
2024-09-23 09:53:04 -0600

    Se agrega clase Persona

---
+++ Corte1/models/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corte1.NewFolder1
+{
+    internal class Persona
+    {
+        public Persona() { }
+        public string name { get; set; }
+        public string apellido { get; set; }
+        public DateTime fechaNacimiento { get; set; }
+        public string ciudad {  get; set; }
+
+
+
+    }
+}
