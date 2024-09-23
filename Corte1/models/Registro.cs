23a885df0b76b656ea3b2ac9923026273c7c87e1
Gogogomez23 <com.gomez.com@gmail.com>
2024-09-23 09:54:10 -0600

    Se agrega clase Registro

---
+++ Corte1/models/Registro.cs
@@ -0,0 +1,28 @@
+﻿using Corte1.NewFolder1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corte1.models
+{
+    internal class Registro
+    {
+        public Registro() { }
+
+        Persona[] personas = new Persona[30];
+
+        public void agregarPersona(Persona persona)
+        {
+            for (int i = 0; i < personas.Length; i++)
+            {
+                if (personas[i] == null)
+                {
+                    personas[i] = persona;
+                    break;
+                }
+            }
+        }
+    }
+}
